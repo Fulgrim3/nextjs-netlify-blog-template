3ae6772bf09a2627940def3cc9966e05ceafb618
Donald J Ross <54287731+Fulgrim3@users.noreply.github.com>
2022-06-02 16:38:50 -0400

    fixed and added CIG to the about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, CIG<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@cignj</span>
+          <h2>Capital Impact Group</h2>
           <SocialList />
         </div>
       </div>
@@ -32,6 +32,7 @@ export default function Index() {
           font-size: 2.5rem;
           margin: 0;
           font-weight: 500;
+          color:#00693e;
         }
         h2 {
           font-size: 1.75rem;
